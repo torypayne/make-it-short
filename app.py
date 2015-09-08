15a5f0fd4fced18f6f06fb15ca730a243046d959
Victoria Payne <vhpayne@gmail.com>
2015-09-07 18:43:14 -0700

    made dummy routes for all our functions

--- app.py
+++ app.py
@@ -14,6 +14,38 @@ app.secret_key = 'thisisntarealsecretkeybecausethisisademo'
 def index():
 	return render_template("index.html")
 
+@app.route("/new_code")
+def new_code(url):
+	url = request.args.get("url")
+	model.db.connect()
+	code = model.create_code(url)
+	model.db.close()
+	render_template("new_url.html", code=code, url=url)
+
+@app.route("/<code>/visits")
+def url_visits(code):
+	model.db_connect()
+	url_info=model.url_info(code)
+	model.db.close()
+	return render_template("visits.html", url_info=url_info)
+
+@app.route("/recent")
+def recent_urls():
+	model.db_connect()
+	recently_shortened = model.recently_shortened()
+	model.db.close()
+	return render_template("recently_shortened.html", recently_shortened=recently_shortened)
+
+@app.route("/popular")
+def popular():
+	model.db_connect()
+	most_popular = model.most_popular()
+	model.db.close()
+	return render_template("most_popular.html", most_popular=most_popular)
+
+
+
+
 
 @app.errorhandler(404)
 def page_not_found(e):
